a4e845b215571d852025a87e0c99ae77cf1a411e
RatnakiranKankipati <kiranratna18@gmail.com>
2024-05-29 19:40:32 +0530

    new apis are added

--- app.js
+++ app.js
@@ -5,6 +5,33 @@ const app=express();
 app.get("/",async(req,res)=>{
 res.send({msg:"success"})
 })
+
+app.get("/get/user",async(req,res)=>{
+    try{
+        res.send(
+            {
+                status:"success",
+                msg:"working fine"
+            }
+        )
+    }catch(err){
+        console.log("error");
+    }
+
+})
+app.get("/get/new",async(req,res)=>{
+    try{
+        res.send(
+            {
+                status:"success",
+                msg:"fine"
+            }
+        )
+    }catch(err){
+        console.log("error");
+    }
+
+})
 // hello
 app.listen(8000,()=>{
     console.log("server runing on port 8000");
